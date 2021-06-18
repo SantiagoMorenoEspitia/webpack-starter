5ed05c817804a2478d06bf5272eaf1dbdedb5125
Santiago Moreno <santiagomma100@gmail.com>
2021-06-18 11:33:13 -0500

    Inicializacion del proyecto

---
+++ src/js/componentes.js
@@ -0,0 +1,10 @@
+import webpacklogo from '../assets/imgs/webpack-logo.png'
+export const saludar = (nombre)=>{
+    console.log('Creando etiqueta h1');
+    const h1 = document.createElement('h1')
+    h1.innerText = `Hola ${nombre}!!!!!!!!`
+    document.body.append(h1)
+    const img = document.createElement('img')
+    img.src = webpacklogo;
+    document.body.append(img)
+}
